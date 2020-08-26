10ebfed2f92d52b6d0bc892bea70596eacb64440
Ethan Cowan <ethan.cowan@gmail.com>
2020-08-25 21:31:20 -0600

    Add dev scripts to package.json + disable eslint
    Slight configuration changes to make development process easier and to
    hide eslint warnings/errors from webpack build:watch.

--- webpack.config.js
+++ webpack.config.js
@@ -11,15 +11,15 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        enforce: 'pre',
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-        options: {
-          fix: true
-        },
-      },
+      // {
+      //   enforce: 'pre',
+      //   test: /\.jsx$/,
+      //   exclude: /node_modules/,
+      //   loader: 'eslint-loader',
+      //   options: {
+      //     fix: true
+      //   },
+      // },
       {
         test: /\.s[ac]ss$/i,
         use: [
