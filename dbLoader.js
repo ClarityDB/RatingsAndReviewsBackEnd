bb5a0e1c23e1bf72bfd94022c8eb6022fcab23d4
Ethan Cowan <ethan.cowan@gmail.com>
2020-09-03 14:01:02 -0600

    DRY up dataGenerator and rm cruft from dbLoader

--- dbLoader.js
+++ dbLoader.js
@@ -11,32 +11,25 @@ const pool = new Pool({
   connectionString: connectionString,
 })
 
-// const loadReviews = () => {
-
-//   let pathName = path.join(__dirname, '/dataToBeLoaded/reviews.csv');
-
-//   let queryString = `COPY reviews(product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness)
-//   FROM '${pathName}'
-//   DELIMITER ','
-//   CSV HEADER;`
-
-//   return pool
-//     .query(queryString)
-//     .then((response) => console.log("load response: ", response))
-//     .catch((err) => console.log("get reviews error: ", err));
-// }
-
+let startReviewsTruncateTime;
+let endPhotosLoad;
 const loadReviews = () => {
-
+  startReviewsTruncateTime = new Date;
+  let endReviewsTruncateTime;
   let queryString = `TRUNCATE TABLE reviews CASCADE`
 
   pool
     .query(queryString)
-    .then(() => copyReviews())
+    .then(() => {
+      endReviewsTruncateTime = new Date;
+      console.log("\nreviews truncation took", (endReviewsTruncateTime - startReviewsTruncateTime) + 'ms');
+      copyReviews();
+    })
     .catch((err) => console.log("truncate error: ", err));
 
   const copyReviews = () => {
-
+    let startReviewsLoad = new Date;
+    let endReviewsLoad;
     let pathName = path.join(__dirname, '/dataToBeLoaded/reviews.csv');
 
     // let queryTruncateString = `TRUNCATE TABLE reviews_photos CASCADE;`
@@ -48,25 +41,33 @@ const loadReviews = () => {
 
     pool
       .query(queryString)
-      .then((response) => console.log("load response: ", response))
+      .then((response) => {
+        endReviewsLoad = new Date;
+        console.log(`loaded ${response.rowCount} review rows in ${(endReviewsLoad - startReviewsLoad)} ms
+        ` )
+    })
       .catch((err) => console.log("get reviews error: ", err));
   }
 }
 
 const loadCharacteristics = () => {
-
+  let startCharacteristicsTruncateTime = new Date;
+  let endCharacteristicsTruncateTime;
   let queryString = `TRUNCATE TABLE characteristics_reviews CASCADE`
 
   pool
     .query(queryString)
-    .then(() => copyCharacteristics())
+    .then(() => {
+      endCharacteristicsTruncateTime = new Date;
+      console.log("\ncharaceteristics truncation took", (endCharacteristicsTruncateTime - startCharacteristicsTruncateTime) + 'ms');
+      copyCharacteristics();
+    })
     .catch((err) => console.log("truncate error: ", err));
 
   const copyCharacteristics = () => {
-
+    let startCharacteristicsLoad = new Date;
+    let endCharacteristicsLoad;
     let pathName = path.join(__dirname, '/dataToBeLoaded/characteristics_reviews.csv');
-
-    // let queryTruncateString = `TRUNCATE TABLE reviews_photos CASCADE;`
 
     let queryString = `COPY characteristics_reviews(id, characteristics_name, review_id, value)
       FROM '${pathName}'
@@ -75,25 +76,32 @@ const loadCharacteristics = () => {
 
     pool
       .query(queryString)
-      .then((response) => console.log("load response: ", response))
+      .then((response) => {
+        endCharacteristicsLoad = new Date;
+        console.log(`loaded ${response.rowCount} characteristics rows in ${(endCharacteristicsLoad - startCharacteristicsLoad)} ms
+        ` )
+    })
       .catch((err) => console.log("get reviews error: ", err));
   }
 }
 
 const loadPhotos = () => {
-
+  let startPhotosTruncateTime = new Date;
+  let endPhotosTruncateTime;
   let queryString = `TRUNCATE TABLE reviews_photos CASCADE`
 
   pool
     .query(queryString)
-    .then(() => copyPhotos())
+    .then(() => {
+      endPhotosTruncateTime = new Date;
+      console.log("\nphotos truncation took", (endPhotosTruncateTime - startPhotosTruncateTime) + 'ms');
+      copyPhotos();
+    })
     .catch((err) => console.log("truncate error: ", err));
 
   const copyPhotos = () => {
-
+    let startPhotosLoad = new Date;
     let pathName = path.join(__dirname, '/dataToBeLoaded/reviews_photos.csv');
-
-    // let queryTruncateString = `TRUNCATE TABLE reviews_photos CASCADE;`
 
     let queryString = `COPY reviews_photos(id, review_id, url)
       FROM '${pathName}'
@@ -102,18 +110,18 @@ const loadPhotos = () => {
 
     pool
       .query(queryString)
-      .then((response) => console.log("load response: ", response))
+      .then((response) => {
+        endPhotosLoad = new Date;
+        console.log(`loaded ${response.rowCount} photo rows in ${(endPhotosLoad - startPhotosLoad)} ms
+        ` )
+        if (response) {
+          console.log(`total load time ${endPhotosLoad - startReviewsTruncateTime} ms`);
+        }
+    })
       .catch((err) => console.log("get reviews error: ", err));
   }
 }
 
-
-
-// const newPhotoLoad =
-//   new Promise(deletePhotos())
-//     .then(() => loadPhotos())
-//     .catch((err) => console.log("error in photo delete: ", err));
-// ;
 loadReviews();
 loadCharacteristics();
 loadPhotos();
